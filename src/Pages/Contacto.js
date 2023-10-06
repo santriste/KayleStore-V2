2ef91a982e1d63a833dccf6a258328c8ec8fd8c8
santriste <santiagoestevez722@gmail.com>
2023-10-06 20:10:01 -0300

    Finished

--- src/Pages/Contacto.js
+++ src/Pages/Contacto.js
@@ -1,30 +1,35 @@
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 import "./Contacto.css";
 
 const Contacto = () => {
+  const { register, handleSubmit } = useForm();
+  const [contactomensaje, setcontactomensaje] = useState(false);
 
-    const { register, handleSubmit } = useForm();
-
-    const enviar = (data) => {
-        console.log(data);
-    }
+  const enviar = (data) => {
+    console.log(data);
+    setcontactomensaje(true);
+  };
 
   return (
-
     <div className="container">
-        <h1 className="main-title">Contacto</h1>
+      <h1 className="main-title">Contacto</h1>
+      {contactomensaje ? (
+        <p className="mensaje-exito">
+          Gracias por contactarte con nosotros, te enviaremos un correo.
+        </p>
+      ) : (
         <form className="formulario" onSubmit={handleSubmit(enviar)}>
-
-            <input type="text" placeholder="Nombre" {...register("nombre")} />
-            <input type="email" placeholder="Email" {...register("email")} />
-            <input type="phone" placeholder="Teléfono" {...register("telefono")} />
-
-            <button className="enviar" type="submit">Enviar</button>
-
+          <input type="text" placeholder="Nombre" {...register("nombre")} />
+          <input type="email" placeholder="Email" {...register("email")} />
+          <input type="tel" placeholder="Teléfono" {...register("telefono")} />
+          <button className="enviar" type="submit">
+            Enviar
+          </button>
         </form>
+      )}
     </div>
+  );
+};
 
-  )
-}
-
-export default Contacto
+export default Contacto;
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,24 +1,22 @@
 import React, { useContext } from "react";
 import { CartContext } from "../Context/CartContext";
 import { Link } from "react-router-dom";
-
+import './Cart.css'
 
 const Cart = () => {
-
     const { cart, precioTotal, vaciarCarrito } = useContext(CartContext);
 
     const handleVaciar = () => {
         vaciarCarrito();
-
     }
 
-    return(
+    return (
         <div className="Container">
             <h1> Carrito </h1>
 
             {
                 cart.map((prod) => (
-                    <div key={prod.id}>
+                    <div key={prod.nombre}>
                         <h3>{prod.nombre}</h3>
                         <p>Precio ${prod.precio}</p>
                         <p>Precio ${prod.precio * prod.cantidad}</p>
@@ -27,21 +25,20 @@ const Cart = () => {
                 ))
             }
 
-        {   
-            cart.length > 0 ? 
-        <>
-            <h2>Precio total: ${precioTotal()}</h2>
-            <button onClick={handleVaciar}>Vaciar</button>
-            <Link to="/checkout">Finalizar compra</Link>
-        </>:
-        <h2>El carrito está vacío</h2>
-
-        }
-
-
+            {
+                cart.length > 0 ?
+                <>
+                    <h2>Precio total: ${precioTotal()}</h2>
+                    <h3>
+                        <Link to="/checkout" className="finish">Finalizar compra</Link>
+                    </h3>
+                    <button className="btnclear" onClick={handleVaciar}>Vaciar</button>
+                </>
+                :
+                <h2>El carrito está vacío</h2>
+            }
         </div>
     )
 }
 
-export default Cart 
-
+export default Cart;
